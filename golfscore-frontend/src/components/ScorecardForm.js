e41756e1b491fd2e3f2016b8d862ecf704d1cec1
bwesterg <benton.westergaard@gmail.com>
2023-06-02 14:37:15 -0600

    opens form to edit hole scores when a scorecard is edited.  Previous functionality is maintained. Still need to make data changed in UPDATE to persist.

--- golfscore-frontend/src/components/ScorecardForm.js
+++ golfscore-frontend/src/components/ScorecardForm.js
@@ -1,110 +1,202 @@
 import React, { Component } from 'react';
 
+// const initialState = {
+//     course_name: "",
+//     tees_name: "",
+//     tees_yardage: "",
+//     players: [{ name: "", scores: Array(18).fill(0) }]
+// };
+
 const initialState = {
     course_name: "",
     tees_name: "",
     tees_yardage: "",
-    players: [{ name: "", scores: Array(18).fill(0) }]
-};
+    players: [{ name: "", scores: Array(18).fill(0) }],
+  };
 
 
 export default class ScorecardForm extends Component {
-  state = initialState;
+    state = initialState;
 
-    componentDidMount(){
-        const {scorecard} = this.props
-        if(this.props.scorecard){
-            const {course_name, tees_name, tees_yardage, players} = scorecard
-            this.setState({
-                course_name, 
+        // componentDidMount(){
+        //     const {scorecard} = this.props
+        //     if(this.props.scorecard){
+        //         const {course_name, tees_name, tees_yardage, players} = scorecard
+        //         this.setState({
+        //             course_name, 
+        //             tees_name,
+        //             tees_yardage, 
+        //             players
+        //         })
+        //     }
+        // }
+
+        componentDidMount() {
+            const { scorecard } = this.props;
+            if (scorecard) {
+              const { id, course_name, tees_name, tees_yardage, players } = scorecard;
+              const updatedPlayers = players.map((player) => {
+                const scores = Array(18).fill(0);
+                for (let i = 1; i <= 18; i++) {
+                  const holeKey = `hole${i}_score`;
+                  if (player.hasOwnProperty(holeKey)) {
+                    scores[i - 1] = player[holeKey];
+                  }
+                }
+                return { id: player.id, name: player.name, scores };
+              });
+              this.setState({
+                id,
+                course_name,
                 tees_name,
-                tees_yardage, 
-                players
-            })
-        }
-    }
+                tees_yardage,
+                players: updatedPlayers,
+              });
+            }
+          }
+          
+          
+          
 
 
-  handleInputChange = (event) => {
-    let { name, value } = event.target;
-    this.setState({ [name]: value });
-  };
 
-  handlePlayerChange = (index, event) => {
-    const { value } = event.target;
-    const { players } = this.state;
-    players[index].name = value;
-    this.setState({ players });
-  };
-
-  handleAddPlayer = () => {
-    this.setState((prevState) => ({
-      players: [...prevState.players, { name: "", scores: Array(18).fill(0) }]
-    }));
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const { course_name, tees_name, tees_yardage, players } = this.state;
-
-    const newScorecard = {
-      course_name,
-      tees_name,
-      tees_yardage,
-      players: players.map(({ name, scores }) => ({
-        name,
-        ...scores.reduce((obj, score, index) => {
-          obj[`hole${index + 1}_score`] = score;
-          return obj;
-        }, {})
-      }))
+    handleInputChange = (event) => {
+        let { name, value } = event.target;
+        this.setState({ [name]: value });
     };
 
-    this.props.addScorecard(newScorecard);
-    this.setState(initialState);
-  };
+    handlePlayerChange = (index, event) => {
+        const { value } = event.target;
+        const { players } = this.state;
+        players[index].name = value;
+        this.setState({ players });
+    };
 
-  render() {
-    const { course_name, tees_name, tees_yardage, players } = this.state;
+    handleScoreChange = (playerIndex, holeIndex, event) => {
+        const { players } = this.state;
+        const updatedPlayers = [...players];
+        updatedPlayers[playerIndex].scores[holeIndex] = parseInt(event.target.value);
+        this.setState({ players: updatedPlayers });
+      };
+      
 
-    return (
-      <form className="scorecard-form" onSubmit={this.handleSubmit}>
-        {this.props.scorecard ? <h3>EDIT this Scorecard</h3> : <h3>Create New Scorecard</h3> }
-        <label>Course</label>
+    handleAddPlayer = () => {
+        this.setState((prevState) => ({
+        players: [...prevState.players, { name: "", scores: Array(18).fill(0) }]
+        }));
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const { course_name, tees_name, tees_yardage, players } = this.state;
+
+        const newScorecard = {
+        course_name,
+        tees_name,
+        tees_yardage,
+        players: players.map(({ name, scores }) => ({
+            name,
+            ...scores.reduce((obj, score, index) => {
+            obj[`hole${index + 1}_score`] = score;
+            return obj;
+            }, {})
+        }))
+        };
+
+        this.props.addScorecard(newScorecard);
+        this.setState(initialState);
+    };
+
+    
+render() {
+  const { course_name, tees_name, tees_yardage, players } = this.state;
+
+  return (
+    // <form className="scorecard-form" onSubmit={this.handleSubmit}>
+
+    <form
+        className="scorecard-form"
+        onSubmit={this.props.scorecard ? this.handleInputChange : this.handleSubmit}
+    >
+      {this.props.scorecard ? (
+        <h3>EDIT this Scorecard</h3>
+      ) : (
+        <h3>Create New Scorecard</h3>
+      )}
+      <label>Course</label>
+      <input
+        type="text"
+        name="course_name"
+        value={course_name}
+        onChange={this.handleInputChange}
+      />
+      <label>Tees</label>
+      <input
+        type="text"
+        name="tees_name"
+        value={tees_name}
+        onChange={this.handleInputChange}
+      />
+      <label>Distance (yards)</label>
+      <input
+        type="number"
+        name="tees_yardage"
+        value={tees_yardage}
+        onChange={this.handleInputChange}
+      />
+      <label>Players</label>
+      {players.map((player, playerIndex) => (
         <input
           type="text"
-          name="course_name"
-          value={course_name}
-          onChange={this.handleInputChange}
+          name={`player-${playerIndex}`}
+          value={player.name}
+          onChange={(event) => this.handlePlayerChange(playerIndex, event)}
+          key={playerIndex}
         />
-        <label>Tees</label>
-        <input
-          type="text"
-          name="tees_name"
-          value={tees_name}
-          onChange={this.handleInputChange}
-        />
-        <label>Distance (yards)</label>
-        <input
-          type="number"
-          name="tees_yardage"
-          value={tees_yardage}
-          onChange={this.handleInputChange}
-        />
-        <label>Players</label>
-        {players.map((player, index) => (
-          <input
-            type="text"
-            name={`player-${index}`}
-            value={player.name}
-            onChange={(event) => this.handlePlayerChange(index, event)}
-            key={index}
-          />
-        ))}
-        <button type="button" onClick={this.handleAddPlayer}>Add Another Player</button>
-        <input type="submit" value="Create Scorecard" />
-      </form>
-    );
-  }
+      ))}
+
+      {/* this is where I will show the hole scores if someone is editing a scorecard */}
+      {this.props.scorecard ? (
+        <div className="hole-score-edit">
+          <table>
+            <thead>
+              <tr>
+                <th>Player</th>
+                {Array.from({ length: 18 }, (_, index) => (
+                  <th key={index}>Hole {index + 1} score</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {players.map((player, playerIndex) => (
+                <tr key={playerIndex}>
+                  <td>{player.name}</td>
+                  {player.scores.map((score, holeIndex) => (
+                    <td key={holeIndex}>
+                      <input
+                        type="number"
+                        className="edit-hole-score"
+                        value={score}
+                        onChange={(event) =>
+                          this.handleScoreChange(playerIndex, holeIndex, event)
+                        }
+                      />
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : null}
+
+      <button type="button" onClick={this.handleAddPlayer}>
+        Add Another Player
+      </button>
+      <input type="submit" value="SUBMIT" />
+    </form>
+  );
+}
+
 }
 
