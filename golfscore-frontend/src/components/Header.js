5c4da951256c50ba5d124a3c557beec94b824ca6
bwesterg <benton.westergaard@gmail.com>
2023-06-11 20:15:36 -0600

    fixes sticky header

--- golfscore-frontend/src/components/Header.js
+++ golfscore-frontend/src/components/Header.js
@@ -1,17 +1,5 @@
 import React from 'react';
 
-
-// var header = document.getElementById("myHeader");
-// var sticky = header.offsetTop;
-
-// function myFunction() {
-//   if (window.pageYOffset > sticky) {
-//     header.classList.add("sticky");
-//   } else {
-//     header.classList.remove("sticky");
-//   }
-// }
-
 export default function Header() {
     return (
         <header className="header" id="myHeader">
---
+++ golfscore-frontend/src/components/helpers/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+const scorecardUrl = "http://127.0.0.1:3000/scorecards/";
+
+
+export function patchScorecard(updatedScorecard) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
